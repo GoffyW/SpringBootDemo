b94ad7b4461a00bbacbd0945b8928e002f80133f
guofei <1451215359@qq.com>
2019-11-21 19:02:38 +0800

    添加日志

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.bean.UserEntity;
 import com.example.demo.jpa.UserJPA;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
+    private final static Logger logger  = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserJPA userJPA;
 
@@ -24,6 +27,7 @@ public class UserController {
      */
     @RequestMapping(value = "/list",method = RequestMethod.GET)
     public List<UserEntity> list(){
+        logger.info("访问到了list请求");
         return userJPA.findAll();
     }
 
